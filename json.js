f71577db31c343a6a9d219a319981d4d2b8c6410
Matius54 <mat54200454@gmail.com>
2024-02-02 17:12:53 -0400

    add more data, and animations :)

--- json.js
+++ json.js
@@ -1,4 +1,5 @@
 async function fetchJSON(url){
-    const response = await fetch(url);
+    const response = await fetch(url,{"cache-control":"no-cache"});
+    if(!response.ok)throw new Error(`Fech error: HTTP code: ${response.status} ${response.statusText}`);
     return await response.json();
 }
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const base = document.querySelector(".data");
 const names = {
     youtube:"Canales de YouTube",
-    links:"links"
+    links:"Enlaces variados"
 }
 
 fetchJSON("./data.json").then((json)=>{
+    base.querySelectorAll("*").forEach(e=>e.remove());
+    const animationExecuted = !sessionStorage.getItem("animationExecuted");
     for(const category in json){
         const divEx = document.createElement("div");
         divEx.setAttribute("class",category);
@@ -13,28 +15,33 @@ fetchJSON("./data.json").then((json)=>{
         divEx.appendChild(title);
         const ul = document.createElement("ul");
         
-        json[category].forEach(element => {
-            
+        json[category].forEach((element,indx) => {
+            let containerStyle = "";
             const container = document.createElement("li");
             if(element.banner){
-                container.setAttribute("style",
-                    `background-image: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("${element.banner}");
+                containerStyle += 
+                    `
+                    background-image: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("${element.banner}");
                     background-position: center; /* Center the image */
                     background-repeat: no-repeat; /* Do not repeat the image */
-                    background-size: cover; /* Resize the background image to cover the entire container */`
-                );
+                    background-size: cover; /* Resize the background image to cover the entire container */
+                    `;
             }
+            if(animationExecuted){
+                containerStyle += `animation-delay: ${indx * 100}ms;`
+            }else{
+                containerStyle += "animation-duration: 0ms;"
+            }
+            container.setAttribute("style",containerStyle);
             container.setAttribute("class","cnt");
-            
-            if(category==="youtube"){
-                const img = document.createElement("img");
-                if(element.image){
-                    img.setAttribute("src",element.image);
-                }else{
-                    img.setAttribute("src","./youtube.svg")
-                }
-                container.appendChild(img);
+
+            const img = document.createElement("img");
+            if(element.icon){
+                img.setAttribute("src",element.icon);
+            }else{
+                img.setAttribute("src","./youtube.svg")
             }
+            container.appendChild(img);
     
             const link = document.createElement("a");
             link.setAttribute("href",element.link);
@@ -53,9 +60,28 @@ fetchJSON("./data.json").then((json)=>{
                 });
                 container.appendChild(tags);
             }
+            const lang = document.createElement("span");
+            lang.setAttribute("class","lang");
+            const langStr = element.lang.startsWith("es")? "Español": "Inglés";
+            lang.innerText=`Idioma: ${langStr}`;
+            container.appendChild(lang);
+
             ul.appendChild(container);
         });
         divEx.appendChild(ul);
         base.appendChild(divEx);
     }
+    if(animationExecuted)sessionStorage.setItem("animationExecuted","true");
+}).catch((e)=>{
+    const err = document.createElement("div");
+    err.setAttribute("class","cnt");
+    const mess = document.createElement("span");
+    mess.innerText = `Ha ocurrido un error: ${e}`;
+    err.appendChild(mess);
+    const reload = document.createElement("button");
+    reload.innerText = "reintentar";
+    reload.setAttribute("onclick","location.reload()");
+    err.append(reload);
+    base.appendChild(err);
+    throw e;
 });
